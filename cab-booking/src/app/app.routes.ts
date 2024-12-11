7a3250cc5a3935be57f2f7ce8d130ca5d0a621db
Siva0717 <sivananthansivananthan533@gmail.com>
2024-12-11 20:28:44 +0530

    Recent Commit
    Recent

--- cab-booking/src/app/app.routes.ts
+++ cab-booking/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+<<<<<<< Updated upstream
     {
         path: 'login',
         loadComponent: ()=>import('./login/login.component').then(c => c.LoginComponent)
@@ -9,4 +12,15 @@ export const routes: Routes = [
         path: 'register',
         loadComponent: ()=>import('./register/register.component').then(c=>c.RegisterComponent)
     }
+=======
+  {
+    path:'',
+    component:AppComponent,
+  },
+  {
+    path:'login',
+    component:LoginComponent,
+  },
+
+>>>>>>> Stashed changes
 ];
---
+++ cab-booking/src/app/role/role.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role',
+  imports: [],
+  standalone:true,
+  templateUrl: './role.component.html',
+  styleUrl: './role.component.css'
+})
+export class RoleComponent {
+
+}
