776637c78ac2c734fbe802acb5e378b610d8cbf0
snappedrat <ahamedrmdind@gmail.com>
2024-12-12 21:31:15 +0530

    booking components

--- cab-booking/src/app/components/navbar/navbar.component.ts
+++ cab-booking/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { PersonalDetailsModalComponent } from './personal-details-modal/personal-details-modal.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,12 +10,30 @@ import { PersonalDetailsModalComponent } from './personal-details-modal/personal
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
-  constructor(private router: Router){}
+  
+  constructor(private router: Router, private route: ActivatedRoute, private auth: AuthService){}
+
+  ngOnInit(): void {
+  }
+
   logout(){
     localStorage.clear()
     this.router.navigate(['/role'])
+  }
+  routeToBooking(){
+    let role = localStorage.getItem('role')
+    if(role == 'user'){
+      this.router.navigate(['user-booking'], {relativeTo: this.route})
+    }
+    else{
+      this.router.navigate(['driver-ride'], {relativeTo: this.route})
+    }
+  }
+
+  getUserDetails(){
+    return this.auth.getUserDetails()
   }
 
 }
--- cab-booking/src/app/components/navbar/personal-details-modal/personal-details-modal.component.ts
+++ cab-booking/src/app/components/navbar/personal-details-modal/personal-details-modal.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-personal-details-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './personal-details-modal.component.html',
   styleUrl: './personal-details-modal.component.css'
 })
-export class PersonalDetailsModalComponent {
+export class PersonalDetailsModalComponent implements OnInit {
+  @Input() userDetails: any
+  userForm:FormGroup
+  constructor(private fb: FormBuilder, private auth: AuthService){
+    this.userForm = this.fb.group({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      username: new FormControl('')
+    })
+  }
+
+  ngOnInit(): void {
+    console.log(this.userDetails);
+
+      this.userForm.get('username')?.setValue(this.userDetails.username)
+      this.userForm.get('email')?.setValue(this.userDetails.email)
+      this.userForm.get('name')?.setValue(this.userDetails.name)
+  }
+
+  submit(){
+    this.auth.setUserDetails(this.userForm.value)
+  }
 
 }
