4b2ac4c4a7cec35a0ea456fdd1c6bcc4350a111c
snappedrat <ahamedrmdind@gmail.com>
2024-12-12 19:55:57 +0530

    sample commit

--- cab-booking/src/app/components/navbar/personal-details-modal/personal-details-modal.component.ts
+++ cab-booking/src/app/components/navbar/personal-details-modal/personal-details-modal.component.ts
@@ -8,18 +8,4 @@ import { Component } from '@angular/core';
 })
 export class PersonalDetailsModalComponent {
 
-  toggleEdit(field:any) {
-    const textElement:any = document.getElementById(`${field}-text`);
-    const inputElement:any = document.getElementById(`${field}-input`);
-
-    if (textElement.classList.contains('d-none')) {
-      textElement.textContent = inputElement.value;
-      textElement.classList.remove('d-none');
-      inputElement.classList.add('d-none');
-    } else {
-      textElement.classList.add('d-none');
-      inputElement.classList.remove('d-none');
-      inputElement.focus();
-    }
-  }
 }
