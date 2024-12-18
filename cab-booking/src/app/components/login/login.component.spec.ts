95b93349ba1b90d96fb708754616beaaaa6bbb17
Siva0717 <sivananthansivananthan533@gmail.com>
2024-12-18 20:22:43 +0530

    commit
    commit

--- cab-booking/src/app/components/login/login.component.spec.ts
+++ cab-booking/src/app/components/login/login.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { LoginComponent} from './login.component';
+
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../app.routes';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +12,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent],
+      providers:[provideHttpClient(),provideRouter(routes)],
     })
     .compileComponents();
 
--- cab-booking/src/app/components/register/register.component.spec.ts
+++ cab-booking/src/app/components/register/register.component.spec.ts
@@ -1,14 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
-
+import { provideHttpClient } from '@angular/common/http';
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent]
+      imports: [RegisterComponent],
+      providers:[provideHttpClient()],
     })
     .compileComponents();
 
